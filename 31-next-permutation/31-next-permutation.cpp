8fd60a26a9850b697016cdcd86a0506511a255e6
SHUBHAM DHAKAD <73836613+Shubhamdhakad432@users.noreply.github.com>
2022-08-11 22:16:58 +0530

    Time: 8 ms (45.74%), Space: 12.2 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,46 @@
 class Solution {
-public:    
+public:
+    void rev(vector<int>&v,int a,int b)
+    {
+        while(a<=b)
+        {
+            swap(v[a],v[b]);
+            a++;
+            b--;
+        }
+    }
+    
     void nextPermutation(vector<int>& v) {
-        next_permutation(v.begin(),v.end());
+        bool srt=true;
+        int a=-1;
+        int n=v.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            if(v[i]<v[i+1]) 
+            {
+                a=i;
+                srt=false;
+                break;
+            }
+        }
+        
+        if(srt)
+        {
+            reverse(v.begin(),v.end());
+            return;
+        }
+        
+        int b=-1;
+        for(int i=n-1;i>=a;i--)
+        {
+            if(v[a]<v[i])
+            {
+                b=i;
+                break;
+            }
+        }
+        
+        swap(v[a],v[b]);
+        reverse(v.begin()+a+1,v.end());
     }
 };
